f282044c3542096806fe5d8948c0e5f2c3a24459
Wouter Bollaert <Wouter.Bollaert@kapture.be>
2019-10-23 14:27:53 +0200

    AT-10 Implementing DAO layer: - Fixed AutoWired issues.

--- src/main/java/be/kapture/dao/AbstractDao.java
+++ src/main/java/be/kapture/dao/AbstractDao.java
@@ -3,12 +3,16 @@ package be.kapture.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-
+@Component
 public abstract class AbstractDao<T> implements Dao<T> {
+	private final SessionFactory sessionFactory;
 
 	@Autowired
-	private SessionFactory sessionFactory;
+	public AbstractDao(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
 	private Session getSession() {
 		return sessionFactory.getCurrentSession();
